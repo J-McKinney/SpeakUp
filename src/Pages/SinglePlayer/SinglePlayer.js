8c6358c4810107a5a7d20dc41ffdaec86a0649f6
Jesse <jessemckinney513@gmail.com>
2021-07-28 12:01:56 -0400

    initial commit

---
+++ src/Pages/SinglePlayer/SinglePlayer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Styles from "./SinglePlayer.module.css";
+
+class SinglePlayer extends Component {
+  state = {};
+
+  render() {
+    return (
+      <>
+        <div className={Styles.wrapper}></div>
+      </>
+    );
+  }
+}
+
+export default SinglePlayer;

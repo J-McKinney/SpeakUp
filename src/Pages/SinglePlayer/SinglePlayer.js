2dbd74f2cd36c897a9e6ea163dbd3fa4959e49b0
Jesse <jessemckinney513@gmail.com>
2021-07-28 12:12:15 -0400

    yay

--- src/Pages/SinglePlayer/SinglePlayer.js
+++ src/Pages/SinglePlayer/SinglePlayer.js
@@ -4,6 +4,16 @@ import Styles from "./SinglePlayer.module.css";
 class SinglePlayer extends Component {
   state = {};
 
+  componentDidMount() {
+    console.log("Mount: ");
+  }
+  componentWillUnmount() {
+    console.log("Unmount: ");
+  }
+  componentDidUpdate() {
+    console.log("Update: ");
+  }
+
   render() {
     return (
       <>

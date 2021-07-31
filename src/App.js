f0543c362c65ff5a6ea7f861e3d7ae19cb1971ec
Jesse <jessemckinney513@gmail.com>
2021-07-31 09:53:49 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-// import SinglePlayer from "./Pages/SinglePlayer/SinglePlayer";
-import Animated from "./Pages/AnimatedBackGround/AnimatedBG";
-// import logo from "./logo.svg";
+import SinglePlayer from "./Pages/SinglePlayer/SinglePlayer";
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
       <div className="App">
-        {/* <header className="App-header"> */}
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <SinglePlayer /> */}
-        <Animated />
-        {/* </header> */}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <SinglePlayer />
+        </header>
       </div>
     </>
   );
